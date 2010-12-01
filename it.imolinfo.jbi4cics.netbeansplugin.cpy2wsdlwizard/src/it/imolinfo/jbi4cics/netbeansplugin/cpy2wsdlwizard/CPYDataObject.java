c6d0a1a5e20e652a789e989cdabfc718d537a598
Vladimir Yaroslavskiy <yaroslavskiy@netbeans.org>
2010-12-01 16:48:59 +0300

    Add it.imolinfo modules

---
+++ it.imolinfo.jbi4cics.netbeansplugin.cpy2wsdlwizard/src/it/imolinfo/jbi4cics/netbeansplugin/cpy2wsdlwizard/CPYDataObject.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright (c) 2005, 2006 Imola Informatica.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the LGPL License v2.1
+ *  which accompanies this distribution, and is available at
+ *  http://www.gnu.org/licenses/lgpl.html
+ */
+
+
+package it.imolinfo.jbi4cics.netbeansplugin.cpy2wsdlwizard;
+
+import java.io.IOException;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataNode;
+import org.openide.loaders.MultiDataObject;
+import org.openide.loaders.MultiFileLoader;
+import org.openide.nodes.Children;
+import org.openide.nodes.CookieSet;
+import org.openide.nodes.Node;
+import org.openide.text.DataEditorSupport;
+import org.openide.util.Lookup;
+
+public class CPYDataObject extends MultiDataObject {
+
+    public CPYDataObject(final FileObject pf, MultiFileLoader loader)
+            throws IOException {
+        super(pf, loader);
+        CookieSet cookies = getCookieSet();
+        cookies.add((Node.Cookie) DataEditorSupport.create(this, getPrimaryEntry(), cookies));
+    }
+
+    @Override
+    protected Node createNodeDelegate() {
+        return new DataNode(this, Children.LEAF, getLookup());
+    }
+
+    @Override
+    public Lookup getLookup() {
+        return getCookieSet().getLookup();
+    }
+}
