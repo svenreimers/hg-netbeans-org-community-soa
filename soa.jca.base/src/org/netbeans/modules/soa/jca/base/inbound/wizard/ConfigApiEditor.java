3e2a9013a2c523be0fbaf4b6c8cdd1dcdb88fffa
sabdulla <sabdulla@netbeans.org>
2011-01-28 12:29:14 -0800

    6867177/6.3 n mask password fields in JCA MDBs

--- soa.jca.base/src/org/netbeans/modules/soa/jca/base/inbound/wizard/ConfigApiEditor.java
+++ soa.jca.base/src/org/netbeans/modules/soa/jca/base/inbound/wizard/ConfigApiEditor.java
@@ -159,7 +159,53 @@ public class ConfigApiEditor extends java.beans.PropertyEditorSupport {
             if (encrypted) {
                 String key = parameterElement.getAttribute("key"); // NOI18N
                 final String decryptedKey = ScEncrypt.decrypt("as8%232.nJXIN384103 bp5md-+z2123", key); // NOI18N
-                Node.Property p = new PropertySupport.ReadWrite(
+                if ("Password".equalsIgnoreCase(name)){ // Mask Password Field
+                	Node.Property p = new PropertySupport.ReadWrite(
+                            name,
+                            String.class,
+                            displayName,
+                            name
+                            ) {
+                				String maskPassword = null;
+								
+                                public boolean canWrite() {
+                                    return editable;
+                                }
+                                public Object getValue() {
+                                    try {
+                                        String val = valueElementRef.getTextContent();
+                                        if (val == null || val.length() == 0) {
+                                            return val;
+                                        }
+                                        String decryptVal = ScEncrypt.decrypt(decryptedKey, val);
+                                       {
+                                        	maskPassword =  "";
+                                        	for (int i=0;i< decryptVal.length(); i++)
+                                        		maskPassword +=  "*";
+                                        	return maskPassword;
+                                        }
+                                    } catch (Exception e) {
+                                        return null;
+                                    }
+                                }
+                                public void setValue(Object val) {
+                                	try {
+	                                	String strVal = (String)val; 
+	                                	if (strVal != null && strVal.length() > 0 && 
+	                                			maskPassword != null && maskPassword.length() >0 && strVal.startsWith(maskPassword)) {
+	                                		strVal = ScEncrypt.decrypt(decryptedKey, valueElementRef.getTextContent())+(strVal.substring(maskPassword.length()));
+	                                	}
+	                                    String encryptVal = ScEncrypt.encrypt(decryptedKey, strVal);
+	                                    valueElementRef.setTextContent(encryptVal);
+	                                    writeValueToXml();
+                                	} catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                    };
+                    pset.put(p);
+                    } else {
+                    	Node.Property p = new PropertySupport.ReadWrite(
                         name,
                         String.class,
                         displayName,
@@ -175,12 +221,6 @@ public class ConfigApiEditor extends java.beans.PropertyEditorSupport {
                                         return val;
                                     }
                                     String decryptVal = ScEncrypt.decrypt(decryptedKey, val);
-                                    if ("Password".equalsIgnoreCase(getName())) {
-                                    	String retVal =  "";
-                                    	for (int i=0;i< decryptVal.length(); i++)
-                                    		retVal +=  "*";
-                                    	return retVal;
-                                    }
                                     return decryptVal;
                                 } catch (Exception e) {
                                     return null;
@@ -193,6 +233,8 @@ public class ConfigApiEditor extends java.beans.PropertyEditorSupport {
                             }
                 };
                 pset.put(p);
+                }
+                
             } else {
                 Node.Property p = new PropertySupport.ReadWrite(
                         name,
